c0f10da833c9ce374ca181dd0e1b9381d9a5903f
cfigueirasvalverde <cfigueirasvalverde@nautilus20.danielcastelao.org>
2018-11-13 11:29:54 +0100

    Boletin9_2

---
+++ NetBeansProjects/Programacion/Boletin9/Boletin9_1/src/boletin9_1/Boletin9_1.java
@@ -0,0 +1,17 @@
+package boletin9_1;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin9_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Contador perita = new Contador();
+        perita.grupoNumeros();
+    }
+    
+}
---
+++ NetBeansProjects/Programacion/Boletin9/Boletin9_2/src/boletin9_2/Boletin9_2.java
@@ -0,0 +1,18 @@
+package boletin9_2;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin9_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    SumaProducto repo = new SumaProducto();
+        System.out.println("Suma: " + repo.suma() + "\nProducto: " 
+                + repo.producto());
+    }
+    
+}
