7063d3b473d5fa016ab0d3b7d263b07054472907
cfigueirasvalverde <cfigueirasvalverde@nautilus20.danielcastelao.org>
2018-11-13 11:37:31 +0100

    examen

---
+++ NetBeansProjects/Programacion/Boletin9/Boletin9_3/src/boletin9_3/Boletin9_3.java
@@ -0,0 +1,17 @@
+package boletin9_3;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin9_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Rectangulo perita = new Rectangulo();
+        System.out.println(perita.calcularArea());
+    }
+    
+}
