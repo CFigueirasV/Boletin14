1fc6ab31faf15c2fcb6f72009b6a13d1608c3b1e
cfigueirasvalverde <cfigueirasvalverde@nautilus20.danielcastelao.org>
2018-11-19 11:31:08 +0100

    Boletin10

---
+++ NetBeansProjects/Programacion/Boletin10/Boletin10_1/src/boletin10/Boletin10.java
@@ -0,0 +1,16 @@
+package boletin10;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Juego lol = new Juego();
+        lol.iniciarJuego();
+    }
+}
